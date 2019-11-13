9a09bc4d41e3a538c74288cc5621e890a4e401f6
Celeste Morton and Liam Kenna <celeste+liam@>
2019-11-13 10:23:16 -0800

    add all files
    Co-Authored-By: Celeste Morton and Liam Kenna <celeste@>

--- src/scripts.js
+++ src/scripts.js
@@ -2,29 +2,29 @@ import $ from 'jquery';
 import 'bootstrap';
 import './css/bootstrap.min.css';
 import './css/styles.css';
+import { Triangle } from './triangle.js';
 
 
-// var number1 = $("#side1").val();
-// var number2 = $("#side2").val();
-// var number3 = $("#side3").val();
+// const number1 = $("#side1").val();
+// const number2 = $("#side2").val();
+// const number3 = $("#side3").val();
 $(document).ready(function(){
   $("form#generator").submit(function(){
-    event.preventDefault()
-    var number1 = parseInt($("#side1").val());
-    var number2 = parseInt($("#side2").val());
-    var number3 = parseInt($("#side3").val());
-    if(number1 === number2 && number2 === number3){
-      $("form").append("<h1> We Generated an 'Equilateral' triangle for you</h1>")
-    } else if (number1 + number2 <= number3 ||
-              number2 + number3 <= number1 ||
-              number1 + number3 <= number2) {
-      $("form").append("<h1>This is not a triangle!!</h1>")
+    event.preventDefault();
+    const number1 = parseInt($("#side1").val());
+    const number2 = parseInt($("#side2").val());
+    const number3 = parseInt($("#side3").val());
+    const thisTriangle = new Triangle(number1, number2, number3);
+    if(thisTriangle.type === 'equilateral') {
+      $("form").append("<h1> We Generated an 'Equilateral' triangle for you</h1>");
+    } else if (thisTriangle.type === 'not a triangle') {
+      $("form").append("<h1>This is not a triangle!!</h1>");
     }
-    else if (number1 === number2 || number2 === number3 || number1 === number3){
-      $("form").append("<h1>We generated an 'Isosceles' triangle for you</h1>")
+    else if (thisTriangle.type === 'isosceles') {
+      $("form").append("<h1>We generated an 'Isosceles' triangle for you</h1>");
     }
-    else if (number1 != number2 && number2 != number3){
-      $("form").append("<h1>We generated a 'Scalene' triangle for you</h1>")
+    else if (thisTriangle.type === 'scalene') {
+      $("form").append("<h1>We generated a 'Scalene' triangle for you</h1>");
     }
   });
-})
+});
